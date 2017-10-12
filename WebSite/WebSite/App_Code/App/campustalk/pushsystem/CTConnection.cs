c100569c2f6b3eb5a8b96fac506659d1db23d5c9
MrshaoGx <a892607124@qq.com>
2017-10-12 13:32:28 +0800

    no

--- WebSite/WebSite/App_Code/App/campustalk/pushsystem/CTConnection.cs
+++ WebSite/WebSite/App_Code/App/campustalk/pushsystem/CTConnection.cs
@@ -25,60 +25,61 @@ public class CTConnection : PersistentConnection
         /***********************2017-09-08  邵国鑫***/
         //1.转发消息到对应id
         //2.连接信息 添加新用户
-
-        try
+        lock (LocObj)
         {
-            CTData<Object> d = new CTData<Object>();
-            d = JsonConvert.DeserializeObject<CTData<Object>>(data);
-            //初始json
-            if (d != null)
+            try
             {
-                switch (d.DataType)
+                CTData<Object> d = new CTData<Object>();
+                d = JsonConvert.DeserializeObject<CTData<Object>>(data);
+                //初始json
+                if (d != null)
                 {
-                    case CTData<Object>.DATATYPE_CONNECTED:
-                        CTData<CTUser> s = JsonConvert.DeserializeObject<CTData<CTUser>>(data);
-                        CTUser user = s.Body;
-                        user.ConnectionId = connectionId;
-                        CTAreaPool.getInstance().addUser(user);
-                        CTUserBase userbase = new CTUserBase();
-                        userbase.Sex = user.Sex;
-                        userbase.Uid = user.Uid;
-                        userbase.School = user.School;
-                        if (!mClients.ContainsKey(connectionId))
-                            mClients.Add(connectionId, userbase);
-                        if (!mFastClients.ContainsKey(user.Uid))
-                            mFastClients.Add(user.Uid, connectionId);
-                        break;
-                    case CTData<Object>.DATATYPE_MESSAGE:
-                        CTData<CTMessage> ctmsg = JsonConvert.DeserializeObject<CTData<CTMessage>>(data);
-                        CTMessage msg = ctmsg.Body;
-                        if (mFastClients.Count > 0 && mFastClients.ContainsKey(msg.To))
-                            Connection.Send(mFastClients[msg.To], data);
-                        /*
-                        
+                    switch (d.DataType)
+                    {
+                        case CTData<Object>.DATATYPE_CONNECTED:
+                            CTData<CTUser> s = JsonConvert.DeserializeObject<CTData<CTUser>>(data);
+                            CTUser user = s.Body;
+                            user.ConnectionId = connectionId;
+                            CTAreaPool.getInstance().addUser(user);
+                            CTUserBase userbase = new CTUserBase();
+                            userbase.Sex = user.Sex;
+                            userbase.Uid = user.Uid;
+                            userbase.School = user.School;
+                            if (!mClients.ContainsKey(connectionId))
+                                mClients.Add(connectionId, userbase);
+                            if (!mFastClients.ContainsKey(user.Uid))
+                                mFastClients.Add(user.Uid, connectionId);
+                            break;
+                        case CTData<Object>.DATATYPE_MESSAGE:
+                            CTData<CTMessage> ctmsg = JsonConvert.DeserializeObject<CTData<CTMessage>>(data);
+                            CTMessage msg = ctmsg.Body;
+                            if (mFastClients.Count > 0 && mFastClients.ContainsKey(msg.To))
+                                Connection.Send(mFastClients[msg.To], data);
+                            /*
 
-                        switch (msg.Type)
-                        {
-                            //文本信息
-                            case CTMessage.MESSAGE_TYPE_TEXT:
-                               
-                                break;
-                            case CTMessage.MESSAGE_TYPE_EMOJI: break;
-                            case CTMessage.MESSAGE_TYPE_AUDIO: break;
-                            case CTMessage.MESSAGE_TYPE_PHOTO: break;
-                        }*/
-                        break;
+
+                            switch (msg.Type)
+                            {
+                                //文本信息
+                                case CTMessage.MESSAGE_TYPE_TEXT:
+
+                                    break;
+                                case CTMessage.MESSAGE_TYPE_EMOJI: break;
+                                case CTMessage.MESSAGE_TYPE_AUDIO: break;
+                                case CTMessage.MESSAGE_TYPE_PHOTO: break;
+                            }*/
+                            break;
+                    }
+
                 }
 
+
             }
-
-
+            catch (Exception e)
+            {
+                string x = e.Message;
+            }
         }
-        catch (Exception e)
-        {
-            string x = e.Message;
-        }
-
         return base.OnReceived(request, connectionId, data);
     }
     protected override bool AuthorizeRequest(IRequest request)
